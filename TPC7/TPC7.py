e5319bb27ede1585c3a2a834fce62107785e3482
josetrib <145260816+josetrib@users.noreply.github.com>
2023-11-21 16:58:22 +0000

    Update TPC7.py
    Adicionadas funções em falta.

--- TPC7/TPC7.py
+++ TPC7/TPC7.py
@@ -1,21 +1,4 @@
-#* Desenvolve as seguintes funcionalidades que não foram trabalhadas na aula:
-#    1. Define uma função que dado um polinómio calcula a sua derivada;
-#    2. Define uma função que recebe dois polinómios `p1` e `p2` e calcula um novo polinómio correspondente à soma de `p1` e `p2`;
-#    3. Define uma função que recebe um polinómio e o desenha num gráfico.
-#
-#* Num ficheiro Python, cria uma aplicação para manipular polinómios com as seguintes operações:
-#done    1. Criar um polinómio interativamente;
-#done    2. Ler uma lista de polinómios de um ficheiro;
-#    3. Listar polinómios: mostra numa tabela os polinómios carregados em memória, adicionando um número de ordem a cada um;
-#    4. Calcular o valor de um polinómio num ponto (usa o número de ordem para referenciar o polinómio);
-#    5. Listar polinómios com grau: à listagem anterior, acrescenta mais uma coluna com o grau;
-#done    6. Maior grau: mostra o polinómio de maior grau e indica o seu número de ordem;
-#    7. Derivada: mostra uma tabela com os polinómios e as respetivas derivadas;
-#    8. Somar dois polinómios: indicando os seus números de ordem;
-#    9. Gerar um gráfico para o polinómio;
-#done    10. Gravar num ficheiro os polinómios em memória;
-#done    0. Sair da aplicação
-
+import matplotlib as plt
 
 listaPol = []
 def criaPolIn():
@@ -46,6 +29,11 @@ def recuperarPolinomios():
     
     fpol.close()    
     return listaPol
+def listarPolinomios():
+    print("Número de ordem\tpolinómio")
+    for ordem, polinomio in listaPol:
+        print(f"{ordem}\t{polinomio}")
+
 
 def grauPolinomio(p):
     grau = -999999999999999999999999999999999999
@@ -53,6 +41,12 @@ def grauPolinomio(p):
         if monomio[1] > grau:
             grau = monomio[1]
     return grau
+def calculoPolinomio():
+    ordem = int(input("Digite o número de ordem do polinómio a ser calculado: "))
+    x = float(input("Digite o valor de x: "))
+    ordem, coeficientes = listaPol[ordem - 1]
+    resultado = sum(c * (x ** i) for i, c in enumerate(coeficientes))
+    print(f"Resultado: {resultado}")
 
 def grauPolinomios(listap):
     maior = -9999999999999
@@ -75,21 +69,112 @@ def guardarPolinomios(listap):
     fpol.close()
     return
 
+def mostrarPolinomiosEGrau():
+    print("Número de ordem\tpolinómio\tGrau")
+    for ordem, coeficientes in listaPol:
+        grau = len(coeficientes) - 1
+        print(f"{ordem}\t{coeficientes}\t{grau}")
+
+def somarPol():
+    ordem1 = int(input("Digite o número de ordem do primeiro polinómio: "))
+    ordem2 = int(input("Digite o número de ordem do segundo polinómio: "))
+    _, coeficientes1 = listaPol[ordem1 - 1]
+    _, coeficientes2 = listaPol[ordem2 - 1]
+    coeficientes_soma = [c1 + c2 for c1, c2 in zip(coeficientes1, coeficientes2)]
+    listaPol.append((ordem, coeficientes_soma))
+    ordem += 1
+    print(f"Resultado da soma (Número de Ordem {ordem}): {coeficientes_soma}")
+
+def derivarPolinomio():
+    polinomioDerivado = []
+    polinomioIndex = int(input("Digite o número de ordem do polinómio: ")) - 1
+    polinomio = listaPol[polinomioIndex]
+    for monomio in polinomio:
+        coeficiente, expoente = monomio
+        novoCoeficiente = coeficiente * expoente
+        novoExpoente = expoente - 1
+        if novoExpoente >= 0:
+            polinomioDerivado.append((novoCoeficiente, novoExpoente))
+    return polinomioDerivado
+
+def gerar_grafico():
+    num_ordem = int(input("Digite o número de ordem do polinómio para gerar o gráfico: "))
+    if 1 <= num_ordem <= len(listaPol):
+        _, coeficientes = listaPol[num_ordem - 1]
+        x = [i for i in range(-10, 11)]
+        y = [sum(c * (xi ** i) for i, c in enumerate(coeficientes)) for xi in x]
+        plt.plot(x, y)
+        plt.xlabel("x")
+        plt.ylabel("f(x)")
+        plt.title(f"Gráfico do polinómio (Número de Ordem {num_ordem})")
+        plt.grid()
+        plt.show()
+    else:
+        print("Número de ordem inválido.")
 
 def main():
+
     print("talcoiso e cenas")
-    escolhaMain = int(input())
+    print("\nEscolha uma opção:")
+    print("1) Criar polinómio")
+    print("2) Ler polinómios de um arquivo")
+    print("3) Mostrar polinómios em memória")
+    print("4) Calcular valor de um polinómio")
+    print("5) Mostrar polinomios com grau")
+    print("6) Mostrar polinómio de maior grau")
+    print("7) Mostrar polinómio de maior grau")
+    print("8) Somar dois polinómios")
+    print("9) Gerar gráfico de um polinómio")
+    print("A) Gravar polinómios em um arquivo")
+    print("0) Sair")
 
-    if escolhaMain == 1:
+    escolhaMain = input("Opção: ")
+
+    if escolhaMain == '1':
         criaPolIn()
-
-    if escolhaMain == 2:
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '2':
         recuperarPolinomios()
-
-    if escolhaMain == 10:
-        guardarPolinomios(listaPol)
-
-    if escolhaMain == 0:
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '3':
+        listarPolinomios()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '4':
+        calculoPolinomio()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '5':
+        mostrarPolinomiosEGrau()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '6':
+        grauPolinomios()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '7':
+        derivarPolinomio()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '8':
+        somarPol()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '9':
+        gerar_grafico()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == 'A':
+        guardarPolinomios()
+        input("\n\nPrima Enter para Voltar")
+        main()
+    elif escolhaMain == '0':
         exit()
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
+        input("\n\nPrima Enter para Voltar")
+        main()
 
 main()
